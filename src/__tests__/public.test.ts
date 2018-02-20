1c4534ba65ca6e95d20043ad44c7ed9b19dbaf21
Rojay Simpson <harrysmith1991@gmail.com>
2018-02-20 00:00:14 -0500

    add jest remove mocha and chia

--- src/__tests__/public.test.ts
+++ src/__tests__/public.test.ts
@@ -7,8 +7,6 @@ import {
     MarketOrderValue,
     Model
 } from '../index'
-import * as chai from "chai";
-const assert = chai.assert;
 
 import { Observable } from 'rxjs';
 
@@ -16,7 +14,7 @@ import { Observable } from 'rxjs';
 describe("BitttrexRx Public Methods", () => {
     let bittrexRx: BittrexRxClient;
 
-    before(done => {
+    beforeAll(done => {
         bittrexRx = new BittrexRxClient();
         done();
     });
@@ -25,71 +23,71 @@ describe("BitttrexRx Public Methods", () => {
         it("should respond with an instance of Market[]", done => {
             bittrexRx.getMarkets()
                 .subscribe(json => {
-                    assert.instanceOf(json[0], Model.Market);
+                    expect(json[0]).toBeInstanceOf(Model.Market);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 
     describe("#getTicker()", () => {
         it("should respond with an instance of Ticker", done => {
             bittrexRx.getTicker('BTC-LTC')
                 .subscribe(json => {
-                    assert.instanceOf(json, Model.Ticker);
+                    expect(json).toBeInstanceOf(Model.Ticker);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 
     describe("#getMarketSummaries()", () => {
         it("should respond with an instance of MarketSummary[]", done => {
             bittrexRx.getMarketSummaries()
                 .subscribe(json => {
-                    assert.instanceOf(json[0], Model.MarketSummary);
+                    expect(json[0]).toBeInstanceOf(Model.MarketSummary);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 
     describe("#getOrderBook()", () => {
         it("should respond with an instance of OrderBook", done => {
             bittrexRx.getOrderBook('BTC-LTC')
                 .subscribe(json => {
-                    assert.instanceOf(json, Model.OrderBook);
+                    expect(json).toBeInstanceOf(Model.OrderBook);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 
     describe("#getMarketHistory()", () => {
         it("should respond with an instance of MarketHistory", done => {
             bittrexRx.getMarketHistory('BTC-LTC')
                 .subscribe(json => {
-                    assert.instanceOf(json[0], Model.MarketHistory);
+                    expect(json[0]).toBeInstanceOf(Model.MarketHistory);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 
     describe("#getCandles()", () => {
         it("should respond with an instance of Candle", done => {
             bittrexRx.getCandles('BTC-LTC', TickIntervalValue.Day)
                 .subscribe(json => {
-                    assert.instanceOf(json[0], Model.Candle);
+                    expect(json[0]).toBeInstanceOf(Model.Candle);
                     done();
                 },
                 done
                 );
-        }).timeout(60000);
+        }, 60000);
     });
 });
